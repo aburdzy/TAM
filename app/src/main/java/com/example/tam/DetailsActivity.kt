2ee7dc15f283968a7912de54ea5cda153ec233dd
aburdzy <80163377+aburdzy@users.noreply.github.com>
2024-01-16 21:17:56 +0100

    style(DetailsActivity): add styles

--- app/src/main/java/com/example/tam/DetailsActivity.kt
+++ app/src/main/java/com/example/tam/DetailsActivity.kt
@@ -4,18 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.tam.ui.theme.TAMTheme
 
 class DetailsActivity : ComponentActivity() {
@@ -54,17 +62,44 @@ fun Showcase(viewModel: DetailsViewModel) {
         else -> {
             uiState.data?.let {
                 val character = it[0]
-                DetailsView(character.id, character.name, character.house, character.actor, character.species, character.image)
+                DetailsView(character.name, character.house, character.actor, character.species, character.image)
             }
         }
     }
 }
 
 @Composable
-fun DetailsView(id: String, name: String, house: String, actor: String, species: String, image: String) {
-    Column(modifier = Modifier.padding(start = 6.dp, end = 6.dp)) {
-        Text(text = house, Modifier.padding(top = 2.dp), fontSize = 8.sp)
-        Text(text = actor, Modifier.padding(top = 2.dp),fontSize = 8.sp)
-        Text(text = species, Modifier.padding(top = 2.dp), fontSize = 8.sp)
+fun DetailsView(name: String, house: String, actor: String, species: String, image: String) {
+    Column {
+        Row(modifier = Modifier.fillMaxWidth().padding(20.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(modifier = Modifier
+                .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                AsyncImage(
+                    model = image,
+                    contentDescription = name,
+                    placeholder = painterResource(id = R.drawable.harry),
+                    modifier = Modifier.size(200.dp)
+                )
+            }
+        }
+        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Text(text = name, Modifier.padding(bottom = 4.dp), fontSize = 20.sp, color = Color.DarkGray)
+        }
+        Row(Modifier.fillMaxWidth().padding(12.dp)) {
+            Column(modifier = Modifier.padding(start = 6.dp, end = 6.dp)) {
+                Text(text = "House:", Modifier.padding(top = 2.dp), fontSize = 16.sp, color = Color.Gray)
+                Text(text = "Actor:", Modifier.padding(top = 2.dp), fontSize = 16.sp, color = Color.Gray)
+                Text(text = "Species:", Modifier.padding(top = 2.dp), fontSize = 16.sp, color = Color.Gray)
+            }
+            Column(modifier = Modifier.padding(start = 6.dp, end = 6.dp)) {
+                Text(text = house, Modifier.padding(top = 2.dp), fontSize = 16.sp)
+                Text(text = actor, Modifier.padding(top = 2.dp),fontSize = 16.sp)
+                Text(text = species, Modifier.padding(top = 2.dp), fontSize = 16.sp)
+            }
+        }
     }
 }
